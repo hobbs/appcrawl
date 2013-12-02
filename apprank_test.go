81975a9dceb3578c370c2296ae55d3660a0d8a1a
Zach Hobbs <zach@tunewiki.com>
2013-12-01 19:57:35 -0800

    Added play store rank test

--- apprank_test.go
+++ apprank_test.go
@@ -16,3 +16,16 @@ func TestAppStoreRank(t *testing.T) {
 		t.Error("Facebook not ranked first for 'facebook' in US App Store")
 	}
 }
+
+func TestPlayStoreRank(t *testing.T) {
+	store := PlayStore{}
+	rank, err := AppRank(store, "facebook", "com.facebook.katana", "US")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if rank != 1 {
+		t.Error("Facebook not ranked first for 'facebook' in US Play Store")
+	}
+}
